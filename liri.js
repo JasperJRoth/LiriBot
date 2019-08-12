660bc922e8a2acbb3d95e9f3b82f38c5ecae5798
JasperJRoth <Jasper.J.Roth@gmail.com>
2019-08-12 13:01:03 -0400

    spotify-this-song and concert-this now work

--- liri.js
+++ liri.js
@@ -12,13 +12,13 @@ var command = process.argv[2];
 var args = process.argv.slice(3);
 
 // processes user input and runs the corresponding command
-function takeInput(command){
+function takeInput(command, args){
     switch(command){
         case "concert-this":
-            console.log("concert-this");
+            concertThis(args.join(" "));
             break;
         case "spotify-this-song":
-            console.log("spotify-this-song");
+            spotifyThisSong(args.join(" "))
             break;
         case "movie-this":
             console.log("movie-this");
@@ -34,10 +34,56 @@ function takeInput(command){
                 message: `${command} is not recognised as a valid command, please enter a valid command\n(Hit enter if you want to cancel)`,
             }).then(function(answers){
                 if(answers.ans != ""){
-                    takeInput(answers.ans);
+                    var input = answers.ans.split(" ");
+                    takeInput(input[0], input.slice(1));
                 }
             });
     }
 }
 
-takeInput(command);
+//searches for the provided band/artist and logs the venue and date of upcoming concerts.
+function concertThis(artist){
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(res){
+        var bandData = res.data;
+        if(bandData.length > 0){
+            console.log("--------------------");
+            bandData.forEach(function(concert){
+                
+                console.log(`Venue name: ${concert.venue.name}`);
+                console.log(`Latitude: ${concert.venue.latitude}`);
+                console.log(`Longitude: ${concert.venue.longitude}`);
+                console.log(`At ${concert.datetime}`);
+                console.log("--------------------");
+            });
+        }else{
+            console.log("Sorry, no results found.");
+        }
+    }).catch(function(error){
+        if(error) throw error;
+    });
+}
+
+function spotifyThisSong(songName){
+    console.log(songName)
+    spotify.search({
+        type: "track",
+        query: songName
+    }).then(function(res){
+        var tracks = res.tracks.items;
+
+        console.log("--------------------");
+        tracks.forEach(function(track){
+            console.log(`Artists: ${track.artists.map(function(artist){
+                return artist.name;
+            }).join(", ")}`)
+            console.log(`Song: ${track.name}`);
+            console.log(`Preview: ${track.preview_url}`);
+            console.log(`Album: ${track.album.name}`);
+            console.log("--------------------");
+        });
+    }).catch(function(error){
+        if(error) throw error;
+    });
+}
+
+takeInput(command, args);
